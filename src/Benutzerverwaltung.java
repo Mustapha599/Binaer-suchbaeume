6947a0ee94496076280b769a8247a4a28f1633c8
belaoumu <belaoun.mustapha@outlook.de>
2025-03-25 13:10:52 +0100

    neu

---
+++ src/Benutzerverwaltung.java
@@ -0,0 +1,21 @@
+public class Benutzerverwaltung {
+
+    public Benutzerverwaltung(){
+
+    }
+
+public void neuenBenutzerAnlegen(String pBenutzername, String pPw){
+
+    }
+
+public void nutzerLoeschen(String pBenutzername, String pPw){
+
+}
+
+public void profilVoranden(String pBenutzername){
+
+}
+
+
+
+}
---
+++ src/utils/Benutzerprofil.java
@@ -0,0 +1,57 @@
+import utils*.;
+public class Benutzerprofil {
+    private String benutzername;
+    private String pw;
+
+    public Benutzerprofil(String login, String ppw) {
+        login = benutzername;
+        ppw = pw;
+    }
+
+    public String getBenutzername() {
+     return benutzername;
+    }
+
+public boolean isGreater(ContentType pContent){
+return true;
+}
+
+public boolean isLess(ContentType pContent){
+        return true;
+}
+
+public boolean (ContentType pContent){
+return true;
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
